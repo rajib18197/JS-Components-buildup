9ccbf0a6afbdf1ed5b47697c52e8b50c8b3f793e
rajib18197 <rajib18197@gmail.com>
2024-08-29 21:05:17 +0600

    initial commit

---
+++ off-canvas-menu-01.starter/js/OffCanvasMenu.jsx
@@ -0,0 +1,93 @@
+import { useLayoutEffect, useRef, useState } from "react";
+
+export default function OffCanvasMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const offsetContainerRef = useRef();
+  const siteContainerRef = useRef();
+  const offsetContainerInitialLeft = useRef();
+  const siteContainerInitialLeft = useRef();
+
+  useLayoutEffect(() => {
+    offsetContainerInitialLeft.current =
+      offsetContainerRef.current.getBoundingClientRect().left;
+    siteContainerInitialLeft.current =
+      siteContainerRef.current.getBoundingClientRect().left;
+  }, []);
+
+  useLayoutEffect(() => {
+    if (isOpen) {
+      offsetContainerRef.current.style.left = `${offsetContainerInitialLeft.current}px`;
+      siteContainerRef.current.transform = `translateX(${siteContainerInitialLeft.current}px)`;
+    } else {
+      offsetContainerRef.current.style.left = `${siteContainerInitialLeft.current}px`;
+      siteContainerRef.current.transform = `translateX(${
+        offsetContainerInitialLeft.current * -1
+      }px)`;
+    }
+  }, [isOpen]);
+
+  return (
+    <div className="app">
+      <div className="offsite-container" ref={offsetContainerRef}>
+        <nav className="nav">
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Work</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className="site-container" ref={siteContainerRef}>
+        <h1>Click the button to open the sidebar!</h1>
+        <button onClick={() => setIsOpen((open) => !open)}>
+          <span>Menu</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+// export default function OffCanvasMenu() {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   return (
+//     <div className={`app ${isOpen ? "offsite-is-open" : ""}`}>
+//       <div className="offsite-container">
+//         <nav className="nav">
+//           <ul>
+//             <li>
+//               <a href="#">Home</a>
+//             </li>
+//             <li>
+//               <a href="#">Work</a>
+//             </li>
+//             <li>
+//               <a href="#">About</a>
+//             </li>
+//             <li>
+//               <a href="#">Contact</a>
+//             </li>
+//           </ul>
+//         </nav>
+//       </div>
+
+//       <div className="site-container">
+//         <h1>Click the button to open the sidebar!</h1>
+//         <button onClick={() => setIsOpen((open) => !open)}>
+//           <span>Menu</span>
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
---
+++ off-canvas-menu-01.starter/js/main.js
@@ -0,0 +1,59 @@
+"use script";
+
+// Implemented by Complete DOM Manipulation. No altering in CSS classes.
+
+// const btnEl = document.querySelector("button");
+// const offsiteContainerEl = document.querySelector(".offsite-container");
+// const siteContainer = document.querySelector(".site-container");
+
+// let offsiteOpen = false;
+
+// const offsiteContainerInitialLeft =
+//   offsiteContainerEl.getBoundingClientRect().left;
+
+// const siteContainerInitialLeft = siteContainer.getBoundingClientRect().left;
+
+// const toggleOffsiteContainer = function () {
+//   if (offsiteOpen) {
+//     offsiteContainerEl.style.left = `${offsiteContainerInitialLeft}px`;
+//     siteContainer.style.transform = `translateX(${siteContainerInitialLeft}px)`;
+//   } else {
+//     offsiteContainerEl.style.left = `${siteContainerInitialLeft}px`;
+//     siteContainer.style.transform = `translateX(${Math.abs(
+//       offsiteContainerInitialLeft
+//     )}px)`;
+//   }
+//   offsiteOpen = !offsiteOpen;
+// };
+
+// btnEl.addEventListener("click", toggleOffsiteContainer);
+
+// Implemented by Toggling Classnames. No direct DOM Manipulation.
+const app = document.querySelector(".app");
+const btnEl = document.querySelector("button");
+const offsiteContainerEl = document.querySelector(".offsite-container");
+const siteContainer = document.querySelector(".site-container");
+
+let isOffsiteOpen = false;
+
+const toggleOffsiteContainer = function () {
+  // 1) Do it with typical classes
+  //   if (app.classList.contains("offsite-is-open")) {
+  //     app.classList.remove("offsite-is-open");
+  //   } else {
+  //     app.classList.add("offsite-is-open");
+  //   }
+
+  // 2) Do it with typical classes but in one-liner
+  //   app.classList.toggle("offsite-is-open");
+
+  //   3) Do it with state variable
+  if (isOffsiteOpen) {
+    app.classList.remove("offsite-is-open");
+  } else {
+    app.classList.add("offsite-is-open");
+  }
+  isOffsiteOpen = !isOffsiteOpen;
+};
+
+btnEl.addEventListener("click", toggleOffsiteContainer);
